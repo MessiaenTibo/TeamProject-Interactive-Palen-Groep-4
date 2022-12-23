540dca3c89a3d2e570fec29d1e22c0722871bd50
TiboMessiaen <tibomessiaen@gmail.com>
2022-12-23 01:22:55 +0100

    Complete new HomePage and 1vs1 page design

--- Front-end/script/app.js
+++ Front-end/script/app.js
@@ -97,8 +97,58 @@ const toggleNav = function() {
     }
   }
 
+const HomePage = function(){
+    btn1VS1.addEventListener('click', function(){
+        console.log("1vs1 clicked");
+    });
+    btnSimonSays.addEventListener('click', function(){
+        console.log("Simon says clicked");
+    });
+    btnSpeedrun.addEventListener('click', function(){
+        console.log("Speedrun clicked");
+    });
+    btnShuttleRun.addEventListener('click', function(){
+        console.log("Shuttle run clicked");
+    });
+    btnSvg1VS1.addEventListener('click', function(){
+        console.log("1vs1 svg clicked");
+    });
+    btnSvgSimonSays.addEventListener('click', function(){
+        console.log("Simon says svg clicked");
+    });
+    btnSvgSpeedrun.addEventListener('click', function(){
+        console.log("Speedrun svg clicked");
+    });
+    btnSvgShuttleRun.addEventListener('click', function(){
+        console.log("Shuttle run svg clicked");
+    });
+    btnInfo1VS1.addEventListener('click', function(){
+        console.log("Info 1vs1 clicked");
+        info1VS1.classList.toggle('o-hide-accessible');
+        btnSvg1VS1.classList.toggle('o-hide-accessible');
+    });
+    btnInfoSimonSays.addEventListener('click', function(){
+        console.log("Info Simon says clicked");
+        infoSimonSays.classList.toggle('o-hide-accessible');
+        btnSvgSimonSays.classList.toggle('o-hide-accessible');
+    });
+    btnInfoSpeedrun.addEventListener('click', function(){
+        console.log("Info Speedrun clicked");
+        infoSpeedrun.classList.toggle('o-hide-accessible');
+        btnSvgSpeedrun.classList.toggle('o-hide-accessible');
+    });
+    btnInfoShuttleRun.addEventListener('click', function(){
+        console.log("Info Shuttle run clicked");
+        infoShuttleRun.classList.toggle('o-hide-accessible');
+        btnSvgShuttleRun.classList.toggle('o-hide-accessible');
+    });
+}
+
 
 const handleDataUI = function () {
+    if(btn1VS1){
+        HomePage();
+    }
     if(btnStart){
         listenToClickKnopen();
     }
@@ -108,6 +158,38 @@ const handleDataUI = function () {
 //#region ***  Init / DOMContentLoaded                  ***********
 const init = function () {
     console.log("DOM geladen");
+
+    /*
+    Page: Home
+    */
+    //Gamemode buttons
+    btn1VS1 = document.querySelector('.js-btn-1vs1');
+    btnSimonSays = document.querySelector('.js-btn-simon-says');
+    btnSpeedrun = document.querySelector('.js-btn-speedrun');
+    btnShuttleRun = document.querySelector('.js-btn-shuttle-run');
+
+    //Gamemode svg buttons
+    btnSvg1VS1 = document.querySelector('.js-card-gamemode-svg-1vs1')
+    btnSvgSimonSays = document.querySelector('.js-card-gamemode-svg-simon-says');
+    btnSvgSpeedrun = document.querySelector('.js-card-gamemode-svg-speedrun');
+    btnSvgShuttleRun = document.querySelector('.js-card-gamemode-svg-shuttle-run');
+
+    //Gamemode Info buttons
+    btnInfo1VS1 = document.querySelector('.js-btn-info-1vs1');
+    btnInfoSimonSays = document.querySelector('.js-btn-info-simon-says');
+    btnInfoSpeedrun = document.querySelector('.js-btn-info-speedrun');
+    btnInfoShuttleRun = document.querySelector('.js-btn-info-shuttle-run');
+
+    //Gamemode Info
+    info1VS1 = document.querySelector('.js-card-gamemode-info-1vs1');
+    infoSimonSays = document.querySelector('.js-card-gamemode-info-simon-says');
+    infoSpeedrun = document.querySelector('.js-card-gamemode-info-speedrun');
+    infoShuttleRun = document.querySelector('.js-card-gamemode-info-shuttle-run');
+
+    /*
+    Page: Game
+    */
+
     btnStart = document.querySelector('.js-btn-start');
     btnPlayer1 = document.querySelector('.js-btn-player1');
     btnPlayer2 = document.querySelector('.js-btn-player2');
